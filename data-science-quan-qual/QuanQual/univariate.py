70fa1ec027cd817bcb816330c5feae1711f9b25b
Jayasurya <jayasuryavaradharaj@gmail.com>
2025-08-28 14:47:21 +0800

    Add files via upload

---
+++ data-science-quan-qual/QuanQual/univariate.py
@@ -0,0 +1,12 @@
+class univariate():
+    def quanqual(dataset):
+                quan=[]
+                qual=[]
+
+                for columnName in dataset.columns:
+                    if (dataset[columnName].dtype=='O'):
+                        qual.append(columnName)
+                    else:
+                        quan.append(columnName)
+                return quan,qual
+   
